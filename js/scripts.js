201a6f042fd4a5ce424b3d1413fe4ac15f044e9a
jssmm <jessenalexmercado@gmail.com>
2020-11-08 00:14:42 -0600

    Variables de JS

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+console.log('Funciona');
+
+//Existen 3 tipos de variables
+var nombre, 
+    edad;
+
+nombre = 'Jessen';
+edad = 23;
+console.log(nombre);
+console.log(edad);
+
+//Con let puedes asignar la variable y cambiarla despues
+let name;
+name = 'Bethsabeé';
+console.log(name);
+
+//con const se tiene que declarar la variable y asignarla
+const client = 'Wichuko';
+console.log(client);
+
+
+let nameTwo;
+nameTwo = 'Entonces dije "hola mundo".'
+
+
+console.log(nameTwo);
